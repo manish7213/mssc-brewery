c4eb55687c1574cb9a19f417e511401f5130b4c4
manish7213 <manish.1si11cs057@gmail.com>
2020-09-02 20:35:27 +0530

    Initial Commit

--- src/main/java/com/manish/sfg/msscbrewery/MsscBreweryApplication.java
+++ src/main/java/com/manish/sfg/msscbrewery/MsscBreweryApplication.java
@@ -1,10 +1,14 @@
 package com.manish.sfg.msscbrewery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsscBreweryApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(MsscBreweryApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBreweryApplication.class, args);
