173057368d162b8da02ffa3c8af9dad1a3fa6673
manish7213 <manish.1si11cs057@gmail.com>
2020-09-08 21:25:52 +0530

    Added MapStruct implementation

--- src/main/java/com/manish/sfg/msscbrewery/domain/Beer.java
+++ src/main/java/com/manish/sfg/msscbrewery/domain/Beer.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 @Data
 @NoArgsConstructor
@@ -16,4 +17,7 @@ public class Beer {
     private String beerName;
     private BeerStyleEnum beerStyle;
     private Long upc;
+
+    private Timestamp createdDate;
+    private Timestamp lastUpdatedDate;
 }
--- src/main/java/com/manish/sfg/msscbrewery/mapper/BeerMapper.java
+++ src/main/java/com/manish/sfg/msscbrewery/mapper/BeerMapper.java
@@ -4,7 +4,7 @@ import com.manish.sfg.msscbrewery.domain.Beer;
 import com.manish.sfg.msscbrewery.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = DateMapper.class)
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
